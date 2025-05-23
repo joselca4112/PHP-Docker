7eae0621d401cbef88252e70a8ab10aabd0bc2cc
joselca4112 <jlcarmo2002@gmail.com>
2025-05-23 15:58:07 +0200

    cosas

--- src/proyecto1PHP/php/web.php
+++ src/proyecto1PHP/php/web.php
@@ -62,54 +62,6 @@
         </ul>
         <hr class="dropdown-divider">
 
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <button class="btn btn-outline-secondary d-none d-md-block w-100 text-center d-flex align-items-center justify-content-center gap-2 small">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                  class="feather feather-calendar">
-                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                  <line x1="16" y1="2" x2="16" y2="6"></line>
-                  <line x1="8" y1="2" x2="8" y2="6"></line>
-                  <line x1="3" y1="10" x2="21" y2="10"></line>
-                </svg>
-                Esta semana
-              </button>
-            </a>
-
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <button class="btn btn-outline-secondary d-none d-md-block w-100 text-center d-flex px-1 align-items-center justify-content-center gap-2 small">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                  class="feather feather-calendar">
-                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                  <line x1="16" y1="2" x2="16" y2="6"></line>
-                  <line x1="8" y1="2" x2="8" y2="6"></line>
-                  <line x1="3" y1="10" x2="21" y2="10"></line>
-                </svg>
-                Semana pasada
-              </button>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <button class="btn btn-outline-secondary d-none d-md-block w-100 text-center d-flex align-items-center justify-content-center gap-2 small">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                  class="feather feather-calendar">
-                  <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-                  <line x1="16" y1="2" x2="16" y2="6"></line>
-                  <line x1="8" y1="2" x2="8" y2="6"></line>
-                  <line x1="3" y1="10" x2="21" y2="10"></line>
-                </svg>
-                Este mes
-              </button>
-            </a>
-          </li>
-        </ul>
       </div>
     </nav>
 
